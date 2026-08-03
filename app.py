00c61116ad9bcb776054eb3aa6031208f9d11483
bgkb8235-art <bgkb8235@gmail.com>
2026-08-03 11:05:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
+# Title
 st.title("Checking the person eligible for vote or not")
 
-age = st.number_input(
-    "Enter your age",
-    min_value=0,
-    max_value=100,
-    step=1
-)
+# Taking the input
+age = st.number_input("Enter your age", min_value=0, step=1)
 
+# Button
 if st.button("Submit"):
-    if age >= 18:
+    if age > 100:
+        st.error("Invalid age input! Please enter an age between 0 and 100.")
+    elif age >= 18:
         st.success("You are eligible to vote.")
     else:
         st.error("You are not eligible to vote.")
