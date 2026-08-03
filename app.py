796406c46e62cc1dc8e59b105ca8407b61a83e79
bgkb8235-art <bgkb8235@gmail.com>
2026-08-03 11:06:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ age = st.number_input("Enter your age", min_value=0, step=1)
 # Button
 if st.button("Submit"):
     if age > 100:
-        st.error("Invalid age input! Please enter an age between 0 and 100.")
+        st.error("Invalid age input!")
     elif age >= 18:
         st.success("You are eligible to vote.")
     else:
